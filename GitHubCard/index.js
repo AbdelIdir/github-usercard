f764ddcc7818a323ee3a67665422615e851adbb0
AbdelIdir <homeworkoracle@protonmail.com>
2019-10-31 16:49:20 +0100

    mvp done and first stretch 1

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,17 +3,6 @@
            https://api.github.com/users/<your name>
 */
 
-
-console.log(`curl -u "AbdelIdir" https://api.github.com`);
-let userInfo = {}
-axios.get("https://lambda-github-api-server.herokuapp.com/")
-.then(response => {
-  const cards = document.querySelector(".cards");
-  cards.appendChild(api(response.data))
-})
-.catch(error => {
- console.error(error)
-});
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -35,15 +24,8 @@ axios.get("https://lambda-github-api-server.herokuapp.com/")
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
-
-
-
-
-
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
-
 
 <div class="card">
   <img src={image url of user} />
@@ -62,75 +44,103 @@ const followersArray = [];
 
 */
 
-function api (obj) {
-console.log(obj);
+function gitCard(gitData) {
+  //// Creating my elements :
+
   const divCard = document.createElement("div");
-  const img = document.createElement("img");
+  const anImg = document.createElement("img");
   const divCardInfo = document.createElement("div");
   const h3 = document.createElement("h3");
-  const pa = document.createElement("p");
-  const pb = document.createElement("p");
-  const pc = document.createElement("p");
-  const a = document.createElement("a");
-  const pd = document.createElement("p");
-  const pe = document.createElement("p");
-  const pf = document.createElement("p");
+  const pUsername = document.createElement("p");
+  const pLocation = document.createElement("p");
+  const pProfile = document.createElement("p");
+  const aGitPage = document.createElement("a");
+  const pFollowers = document.createElement("p");
+  const pFollowing = document.createElement("p");
+  const pBio = document.createElement("p");
 
+  //// Appending my elements to the relevant parent elements :
+
+  divCard.append(anImg);
+  divCard.append(divCardInfo);
+  divCardInfo.append(h3);
+  divCardInfo.append(pUsername);
+  divCardInfo.append(pLocation);
+  divCardInfo.append(pProfile);
+  divCardInfo.append(aGitPage);
+  divCardInfo.append(pFollowers);
+  divCardInfo.append(pFollowing);
+  divCardInfo.append(pBio);
+
+  pProfile.append(aGitPage);
+
+  ////// giving classes to some elements :
 
   divCard.classList.add("card");
   divCardInfo.classList.add("card-info");
-  h3.classList.add("h3");
-  pa.classList.add("username");
+  h3.classList.add("name");
+  pUsername.classList.add("username");
 
-  img.setAttribute ("src", obj["avatar_url"]);
+  ///// Feeding data to the elements :
 
+  anImg.src = gitData.image;
+  h3.textContent = gitData.name;
+  pUsername.textContent = gitData.gender;
+  pLocation.textContent = gitData.location.name;
+  // aGitPage.textContent = gitData.avatar_url;
+  pFollowers.textContent = gitData.species;
+  pFollowing.textContent = gitData.status;
+  // pBio.textContent = gitdata.bio;
 
-h3.textContent = obj.name;
-pa.textContent = obj.login;
-pb.textContent = obj.location;
-
-pd.textContent = obj.followers;
-pe.textContent = obj.following;
-pf.textContent = obj.bio;
-
-const aURL = obj["html_url"];
- a.setAttribute("href", aURL);
- a.href = aURL;
- a.textContent = aURL;
-
-
-
-
-
-//append a to pc here
-pc.appendChild(a);
-
-//                    appending everything to the object;
-
-
-divCard.appendChild(img);
-divCard.appendChild(divCardInfo);
-divCardInfo.appendChild(h3);
-divCardInfo.appendChild(pa);
-divCardInfo.appendChild(pb);
-divCardInfo.appendChild(pc);
-divCardInfo.appendChild(pd);
-divCardInfo.appendChild(pe);
-divCardInfo.appendChild(pf);
-
-
-
-
-
-
-
-
-
-return divCard;
+  return divCard;
 }
+axios
+  .get("https://rickandmortyapi.com/api/character/2")
+  .then(response => {
+    console.log(response);
+    const cardHolder = document.querySelector(".cards");
 
+    cardHolder.append(gitCard(response.data));
+  })
 
+  .catch(error => {
+    console.log(error);
+  });
 
+const followersArray = [
+  // "https://api.github.com/user/estheragbaje",
+  // "https://api.github.com/user/jasynmarais",
+  // "https://api.github.com/user/richanynguon",
+  // "https://api.github.com/user/mikeattara",
+  // "https://api.github.com/user/domeccleston"
+
+  //   "9",
+  //   "3",
+  //   "4",
+  //   "5",
+  //  "6"
+  "character"
+];
+
+// function followers(apiLinks) {
+  // axios.get(`https://api.github.com/user/${apiLinks}`)
+  axios
+    .get(`https://rickandmortyapi.com/api/character`)
+    .then(response => {
+      console.log(response);
+      const cardHolder = document.querySelector(".cards");
+      response.data.results.forEach(info => {
+
+            cardHolder.appendChild(gitCard(info));
+      }
+      )
+    })
+    .catch(error => {
+      console.error(error);
+    });
+// }
+
+// followersArray.forEach(people => followers(people));
 
 /* List of LS Instructors Github username's: 
   tetondan
@@ -138,4 +148,6 @@ return divCard;
   justsml
   luishrd
   bigknell
+
+
 */
